9e657056ab2b58e34136124cd6fe790486c94d10
Daniel <191582347+danhoernchen@users.noreply.github.com>
2025-01-01 20:57:01 +0100

    updated the player with movement via w/s keys

--- player.py
+++ player.py
@@ -13,6 +13,10 @@ class Player(circleshape.CircleShape):
 
     def rotate(self,dt):
         self.rotation += dt * PLAYER_TURN_SPEED
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -28,3 +32,7 @@ class Player(circleshape.CircleShape):
             self.rotate(dt)
         if keys[pygame.K_d]:
             self.rotate(dt*-1)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt*-1)
