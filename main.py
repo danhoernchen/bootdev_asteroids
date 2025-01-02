d86167c4a7d9025dd3269980900d032cd90ae958
Daniel <191582347+danhoernchen@users.noreply.github.com>
2025-01-02 16:34:48 +0100

    added asteroid.py and asterdoidfield.py
    added files, added an asteroidfield to the game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from constants import *
 
 pygame_clock = pygame.time.Clock()
@@ -11,15 +13,24 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (updatable, drawable, asteroids)
+	AsteroidField.containers = (updatable)
 	player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+	asteroid_field = AsteroidField()
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		dt = (pygame_clock.tick(60)/1000)
+		for current_updatable in updatable:
+			current_updatable.update(dt)
 		screen.fill((0,0,0))
-		player.update(dt)
-		player.draw(screen)
+		for current_drawable in drawable:
+			current_drawable.draw(screen)
 		pygame.display.flip()
 
 
--- player.py
+++ player.py
@@ -3,6 +3,7 @@ import pygame
 from constants import *
 
 
+
 class Player(circleshape.CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y, PLAYER_RADIUS)
